4060b57f4f3741d11495cfed21380dea6d2c24bc
Andrea Leonardi <leoandrea14a@gmail.com>
2026-02-23 19:15:19 +0100

    Add files via upload

--- multimodal_glycemic_analysis/Scripts/MLapplications.py
+++ multimodal_glycemic_analysis/Scripts/MLapplications.py
@@ -9,9 +9,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
-from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm import SVC
+from sklearn.feature_selection import SelectFromModel
+
 
 
 # =========================
@@ -19,6 +25,11 @@ from sklearn.linear_model import LogisticRegression
 # =========================
 
 def run_script(path: Path) -> None:
+    """
+    Esegue uno script .py come se fosse lanciato da terminale.
+    Questo fa scattare il blocco:
+        if __name__ == "__main__": main()
+    """
     runpy.run_path(str(path), run_name="__lag__")
 
 if False:
@@ -41,7 +52,8 @@ X = df.drop(columns=[target_col,"bg_mean","bg_sd","bg_cv","bg_min","bg_median","
 # Defining variables
 # =========================
 
-cv = KFold(n_splits=5, shuffle= False)
+cv = TimeSeriesSplit(n_splits=5)
+
 
 
 
@@ -137,7 +149,7 @@ print("PLS Best n_components:", grid.best_params_["pls__n_components"])
 logreg_df= pd.DataFrame(columns=["q", "AUC", "C"])
 
 
-for q in range(25,76, 1):
+for q in [35,50,65]:
     q = q/100
     y_bin = (y >= y.quantile(q)).astype(int)
     #è 1 quando la glicemia va male e 0 quando va bene
@@ -145,7 +157,7 @@ for q in range(25,76, 1):
     
     pipe = Pipeline([
         ("scaler", StandardScaler()),
-        ("logit", LogisticRegression(max_iter=10000))
+        ("logit", LogisticRegression(max_iter=10000, class_weight="balanced"))
         ])
     
     param_grid = {
@@ -171,21 +183,205 @@ print(f"\nBest logistic model -> | quantile = {logregbest.iloc[0,0]} | AUC = {lo
 # =========================
 # 5) Naive Bayes
 # =========================
+
+NB_df= pd.DataFrame(columns=["q", "AUC"])
+
+
+for q in [35,50,65]:
+    q = q/100
+    y_bin = (y >= y.quantile(q)).astype(int)
+    #è 1 quando la glicemia va male e 0 quando va bene
+
+    
+    pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("nb", GaussianNB())
+        ])
+
+    
+    grid = GridSearchCV(
+        pipe,
+        param_grid ={},
+        cv=cv,
+        scoring="roc_auc",   # meglio di accuracy
+        n_jobs=-1
+    )
+    
+    grid.fit(X, y_bin)
+    
+    NB_df.loc[len(NB_df)] = [q,grid.best_score_]
+
+NB_df = NB_df[NB_df["AUC"]==NB_df["AUC"].max()]
+
+print(f"\nBest NaiveBayes -> | quantile = {NB_df.iloc[0,0]} | AUC = {NB_df.iloc[0,1]}")
+
 # =========================
 # 6) LDA
 # =========================
+
+LDA_df= pd.DataFrame(columns=["q", "AUC"])
+
+
+for q in [35,50,65]:
+    q = q/100
+    y_bin = (y >= y.quantile(q)).astype(int)
+    #è 1 quando la glicemia va male e 0 quando va bene
+
+    
+    pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("lda", LinearDiscriminantAnalysis())
+        ])
+
+    
+    grid = GridSearchCV(
+        pipe,
+        param_grid ={},
+        cv=cv,
+        scoring="roc_auc",   # meglio di accuracy
+        n_jobs=-1
+    )
+    
+    grid.fit(X, y_bin)
+    
+    LDA_df.loc[len(LDA_df)] = [q,grid.best_score_]
+
+LDA_df = LDA_df[LDA_df["AUC"]==LDA_df["AUC"].max()]
+
+print(f"\nBest LDA -> | quantile = {LDA_df.iloc[0,0]} | AUC = {LDA_df.iloc[0,1]}")
+
 # =========================
-# 7) SVM
+# 7) SVM 
 # =========================
+SVM_df= pd.DataFrame(columns=["q", "AUC","kernel"])
 
 
+for q in [35,50,65]:
+    q = q/100
+    y_bin = (y >= y.quantile(q)).astype(int)
+    #è 1 quando la glicemia va male e 0 quando va bene
+    
+    pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm", SVC(class_weight="balanced"))  # decision_function disponibile
+    ])
+    
+    param_grid = [
+        # LINEARE
+        {
+            "svm__kernel": ["linear"],
+            "svm__C": np.logspace(-3, 3, 25)
+        },
+        # RBF
+        {
+            "svm__kernel": ["rbf"],
+            "svm__C": np.logspace(-1, 2, 15),
+            "svm__gamma": np.logspace(-4, -1, 15)  # gamma = 1/(2*sigma^2)
+        }
+    ]
+    
+    grid = GridSearchCV(
+        pipe,
+        param_grid=param_grid,
+        cv=cv,
+        scoring="roc_auc",
+        n_jobs=-1
+    )
+    
+    grid.fit(X, y_bin)
+    SVM_df.loc[len(SVM_df)] = [q, grid.best_score_, grid.best_params_["svm__kernel"]]
+
+best = SVM_df.loc[SVM_df["AUC"].idxmax()]
+print(f"Best SVM -> q = {best['q']} | AUC = {best['AUC']} | kernel = {best['kernel']}")
+
+# =========================
+# 7) SVM PCA
+# =========================
+SVM_df= pd.DataFrame(columns=["q", "AUC","kernel"])
 
 
+for q in [35,50,65]:
+    q = q/100
+    y_bin = (y >= y.quantile(q)).astype(int)
+    #è 1 quando la glicemia va male e 0 quando va bene
+    
+    pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("pca", PCA(n_components=10)),
+        ("svm", SVC(class_weight="balanced"))  # decision_function disponibile
+    ])
+    
+    param_grid = [
+        # LINEARE
+        {
+            "svm__kernel": ["linear"],
+            "svm__C": np.logspace(-3, 3, 25)
+        },
+        # RBF
+        {
+            "svm__kernel": ["rbf"],
+            "svm__C": np.logspace(-1, 2, 15),
+            "svm__gamma": np.logspace(-4, -1, 15)  # gamma = 1/(2*sigma^2)
+        }
+    ]
+    
+    grid = GridSearchCV(
+        pipe,
+        param_grid=param_grid,
+        cv=cv,
+        scoring="roc_auc",
+        n_jobs=-1
+    )
+    
+    grid.fit(X, y_bin)
+    SVM_df.loc[len(SVM_df)] = [q, grid.best_score_, grid.best_params_["svm__kernel"]]
+
+best = SVM_df.loc[SVM_df["AUC"].idxmax()]
+print(f"Best SVM (PCA) -> q = {best['q']} | AUC = {best['AUC']} | kernel = {best['kernel']}")
 
 
+# =========================
+# 7) SVM (log selector)
+# =========================
+SVM_df= pd.DataFrame(columns=["q", "AUC","kernel"])
 
 
+for q in [35,50,65]:
+    q = q/100
+    y_bin = (y >= y.quantile(q)).astype(int)
+    #è 1 quando la glicemia va male e 0 quando va bene
+    
+    pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("selector", SelectFromModel(LogisticRegression(l1_ratio=1,solver="liblinear"))),
+        ("svm", SVC(class_weight="balanced"))  # decision_function disponibile
+    ])
+    
+    param_grid = [
+        # LINEARE
+        {
+            "svm__kernel": ["linear"],
+            "svm__C": np.logspace(-3, 3, 25)
+        },
+        # RBF
+        {
+            "svm__kernel": ["rbf"],
+            "svm__C": np.logspace(-1, 2, 15),
+            "svm__gamma": np.logspace(-4, -1, 15)  # gamma = 1/(2*sigma^2)
+        }
+    ]
+    
+    grid = GridSearchCV(
+        pipe,
+        param_grid=param_grid,
+        cv=cv,
+        scoring="roc_auc",
+        n_jobs=-1
+    )
+    
+    grid.fit(X, y_bin)
+    SVM_df.loc[len(SVM_df)] = [q, grid.best_score_, grid.best_params_["svm__kernel"]]
 
-
-
+best = SVM_df.loc[SVM_df["AUC"].idxmax()]
+print(f"Best SVM (log selector) -> q = {best['q']} | AUC = {best['AUC']} | kernel = {best['kernel']}")
 
